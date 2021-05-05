a3b17fed518fdb686acddf01ca6b6c8f3a425ad5
송우현 <lkjfrf@naver.com>
2021-05-05 14:20:34 +0900

    First

---
+++ Cplus_Study/p40.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main()
+{
+	using namespace std;
+
+	int carrots;
+
+	carrots = 25;
+	cout << "나는 당근을 ";
+	cout << carrots;
+	cout << "개를 가지고 있다.";
+	cout << endl;
+	carrots = carrots - 1;
+	cout << "아삭아삭, 이제 당은은 " << carrots << "개이다." << endl;
+
+
+	return 0;
+}
