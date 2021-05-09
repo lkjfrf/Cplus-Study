1d73a42807f3ca9f41360919642077721130583b
송우현 <lkjfrf@naver.com>
2021-05-09 18:00:49 +0900

    5/9

---
+++ Cplus_Study/p151.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+	using namespace std;
+	char charr1[20];
+	char charr2[20] = "jaguaR";
+	string str1;
+	string str2 = "panther";
+	
+	cout << "µ¿¹°";
+	cin >> charr1;
+	cout << "¶Ç : ";
+	cin >> str1;
+
+
+}
+
