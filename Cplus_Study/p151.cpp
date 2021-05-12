819aafc51e2a965692de96853b292b0ae5ecf039
송우현 <lkjfrf@naver.com>
2021-05-12 12:38:34 +0900

    5/11

--- Cplus_Study/p151.cpp
+++ Cplus_Study/p151.cpp
@@ -1,19 +1,19 @@
-#include <iostream>
-#include <string>
-
-int main()
-{
-	using namespace std;
-	char charr1[20];
-	char charr2[20] = "jaguaR";
-	string str1;
-	string str2 = "panther";
-	
-	cout << "동물";
-	cin >> charr1;
-	cout << "또 : ";
-	cin >> str1;
-
-
-}
-
+//#include <iostream>
+//#include <string>
+//
+//int main()
+//{
+//	using namespace std;
+//	char charr1[20];
+//	char charr2[20] = "jaguaR";
+//	string str1;
+//	string str2 = "panther";
+//	
+//	cout << "동물";
+//	cin >> charr1;
+//	cout << "또 : ";
+//	cin >> str1;
+//
+//
+//}
+//
