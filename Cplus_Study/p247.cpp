464787f9c97a9afd48e6710b11151ad394829457
송우현 <lkjfrf@naver.com>
2021-05-16 00:08:38 +0900

    5/15

---
+++ Cplus_Study/p247.cpp
@@ -0,0 +1,14 @@
+//#include <iostream>
+//#include <string>
+//
+//int main()
+//{
+//	using namespace std;
+//	cout << "단어 하나 입력 : ";
+//	string word;
+//	cin >> word;
+//
+//	for (int i = word.size() - 1; i >= 0; i--)
+//		cout << word[i];
+//
+//}
---
+++ Cplus_Study/p281.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{
+	using namespace std;
+	char ch;
+	int count = 0;
+	cout << "문자 입력, 끝낼려면 # 입력 : \n";
+	cin.get(ch);
+	while (ch != '#')
+	{
+		cout << ch;
+		++count;
+		cin.get(ch);
+	}
+	cout << endl << count << " 문자를 읽었습니다.";
+}
