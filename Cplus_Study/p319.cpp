5b1bf471d8a51b1436881033821f907b2c80c4bd
송우현 <lkjfrf@naver.com>
2021-05-18 18:36:17 +0900

    Revert "5/15"
    This reverts commit ba96959f7ec9a3c4cdd8f5a5332af2a425a5a710.

--- Cplus_Study/p319.cpp
+++ Cplus_Study/p319.cpp
@@ -1,34 +1,29 @@
-//#include <iostream>
-//
-//using namespace std;
-//
-//const int ArSize = 6;
-//
-//int main()
-//{
-//	float naaq[ArSize];
-//	cout << "NAAQ지수를 입력하셈 \n" << ArSize << "명의 데이터 모두 입력하거나, 음수를 입력하면 탈출합니다." << endl;
-//
-//	int i = 0;
-//	float temp;
-//
-//	do 
-//	{
-//		if (i <= ArSize)
-//		{
-//			cout << i << "번 값 : ";
-//			cin >> temp;
-//		}
-//
-//		naaq[i] = temp;
-//		++i;
-//	} while (i <= ArSize && temp >= 0);
-//
-//	for (int i = 0; i < ArSize; i++)
-//	{
-//		cout << naaq[i];
-//	}
-//
-//	return 0;
-//}
-//
+#include <iostream>
+
+using namespace std;
+
+const int ArSize = 6;
+
+int main()
+{
+	float naaq[ArSize];
+	cout << "NAAQ지수를 입력하셈 \n" << ArSize << "명의 데이터 모두 입력하거나, 음수를 입력하면 탈출합니다." << endl;
+
+	int i = 0;
+	float temp;
+
+	do while (i <= ArSize && temp >= 0)
+	{
+		if (i <= ArSize)
+		{
+			cout << i << "번 값 : ";
+			cin >> temp;
+		}
+
+		naaq[i] = temp;
+		++i;
+	}
+
+
+}
+
