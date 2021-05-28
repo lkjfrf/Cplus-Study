79cc2f6a2b6394853f159e6578cc3d3c9af2c730
lkjfrf <lkjfrf@naver.com>
2021-05-28 16:39:31 +0900

    5/27

---
+++ Cplus_Study/p550_7.cpp
@@ -0,0 +1,17 @@
+//#include <iostream>
+//
+//template <typename T>
+//T bigger(T a, T b)
+//{
+//	if (a > b)
+//	{
+//		return a;
+//	}
+//	return b;
+//}
+//
+//int main()
+//{
+//
+//	return 0;
+//}
