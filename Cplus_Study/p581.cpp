9cb8c6e0b97daf0260a90e26a619f1971f62fad0
lkjfrf <lkjfrf@naver.com>
2021-07-01 21:41:54 +0900

    6/30

--- Cplus_Study/p581.cpp
+++ Cplus_Study/p581.cpp
@@ -1,18 +1,18 @@
-#include <iostream>
-
-using namespace std;
-
-double warming = 0.3;
-
-void update(double dt);
-void local();
-
-int main()
-{
-	cout << "전역 warming은" << warming << "도입니다." << endl;
-	update(0.1);
-	cout << "전역 warming은" << warming << "도입니다." << endl;
-	local();
-	cout << "전역 warming은" << warming << "도입니다." << endl;
-	return 0;
-}
+//#include <iostream>
+//
+//using namespace std;
+//
+//double warming = 0.3;
+//
+//void update(double dt);
+//void local();
+//
+//int main()
+//{
+//	cout << "전역 warming은" << warming << "도입니다." << endl;
+//	update(0.1);
+//	cout << "전역 warming은" << warming << "도입니다." << endl;
+//	local();
+//	cout << "전역 warming은" << warming << "도입니다." << endl;
+//	return 0;
+//}
---
+++ Cplus_Study/p599.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <new>
+using namespace std;
+
+const int BUF = 512;
+const int N = 5;
+
+char buffer[BUF];
+
+int main()
+{
+	double* pd1, * pd2;
+	int i;
+	cout << "new와 위치지정 new의 첫번째 호출 : \n";
+	pd1 = new double[N];
+	pd2 = new (buffer) double[N];
+
+	for (i = 0; i < N; i++)
+	{
+		pd2[i] = pd1[i] = 1000 + 20.0 * i;
+	}
+	cout << "메모리 주소 : \n" << pd1 << " : 힙;    " << (void*)buffer << ": 정적" << endl;
+	cout << "메모리 내용 : \n";
+	for (i = 0; i < N; i++)
+	{
+		cout << &pd1[i] << "에 " << pd1[i] << ";   ";
+		cout << &pd2[i] << "에 " << pd2[i] << endl;
+	}
+
+	cout << "\nnew와 위치 지정 new의 두번째 호출 : \n";
+	double* pd3, * pd4;
+	pd3 = new double[N];
+	pd4 = new (buffer) double[N];
+	for (i = 0; i < N; i++)
+	{
+		pd4[i] = pd3[i] = 1000 + 40.0 * i;
+	}
+
+	cout << "메모리 내용 : \n";
+	for (i = 0; i < N; i++)
+	{
+		cout << &pd1[i] << "에 " << pd1[i] << ";   ";
+		cout << &pd2[i] << "에 " << pd2[i] << endl;
+	}
+
+
+
+	cout << "\nnew와 위치 지정 new의 세번째 호출 : \n";
+	delete[] pd1;
+	pd1 = new double[N];
+	pd2 = new (buffer + N * sizeof(double)) double[N];
+	for (i = 0; i < N; i++)
+	{
+		pd2[i] = pd1[i] = 1000 + 60.0 * i;
+	}
+
+	cout << "메모리 내용 : \n";
+	for (i = 0; i < N; i++)
+	{
+		cout << &pd1[i] << "에 " << pd1[i] << ";   ";
+		cout << &pd2[i] << "에 " << pd2[i] << endl;
+	}
+	delete[] pd1;
+	delete[] pd3;
+
+}
